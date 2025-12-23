0f7a74ef1be6d44bd06317083495847c99775703
Rishabh Tiwari <mrishabh532@gmail.com>
2025-12-23 16:52:26 +0530

    [Rishabh Tiwari].Add Program to call all other classes that perform operation in .net project.

---
+++ Level_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        //SimpleInterest.Interest();
+        //MaximumHandshakes.Calculate();
+        //NumberCheck.Check();
+        //NaturalSum.Check();
+        //QuotientRemainder.Find();
+        //LargestSmallestNumber.Find();
+        //WindChillCalculator.FindWindChill();
+        TriangularParkRun.FindRun();
+        //TrigonometricFunctions.FindRadian();
+        //ChocolateDivide.Find();
+
+        //if (args.Length != 2)
+        //{
+        //    Console.WriteLine("Please provide month and day.");
+        //    return;
+        //}
+
+        //int month = int.Parse(args[0]);
+        //int day = int.Parse(args[1]);
+
+        //// method call
+        //SpringSeasonOrNot.CheckSpringSeason(month, day);
+
+    }
+}
