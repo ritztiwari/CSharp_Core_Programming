f3405e0af570b4667e09c4fd1ddb434ef32581b4
Rishabh Tiwari <mrishabh532@gmail.com>
2025-12-26 00:56:01 +0530

    [Rishabh Tiwari].Add program to unit converter program 1.

---
+++ Level_02/UnitConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_02
+{
+    // utility class for unit conversions
+    internal class UnitConverter
+    {
+        /*
+Extend or Create a UnitConvertor utility class similar to the one shown in the notes to
+do the following. Please define static methods for all the UnitConvertor class methods. E.g.
+public static double ConvertKmToMiles(double km) =>
+a. Method To convert kilometers to miles and return the value. Use the following
+code double km2miles = 0.621371;
+b. Method to convert miles to kilometers and return the value. Use the following
+code double miles2km = 1.60934;
+c. Method to convert meters to feet and return the value. Use the following code
+to convert double meters2feet = 3.28084;
+d. Method to convert feet to meters and return the value. Use the following code
+to convert double feet2meters = 0.3048
+         */
+
+        public static double ConvertKmToMiles(double km)
+        {
+            double km2miles = 0.621371;
+            return km * km2miles;
+        }
+        public static double ConvertMilesToKm(double miles)
+        {
+            double miles2km = 1.60934;
+            return miles * miles2km;
+        }
+        public static double ConvertMetersToFeet(double meters)
+        {
+            double meters2feet = 3.28084;
+            return meters * meters2feet;
+        }
+        public static double ConvertFeetToMeters(double feet)
+        {
+            double feet2meters = 0.3048;
+            return feet * feet2meters;
+        }
+    }
+}
